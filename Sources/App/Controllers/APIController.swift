2bb99992dda97fc5609629b32f24ba0eca1a590d
unbedenklich <info@unbedenklich.eu>
2024-12-03 23:17:17 +0100

    Fix warning.

--- Sources/App/Controllers/APIController.swift
+++ Sources/App/Controllers/APIController.swift
@@ -6,7 +6,7 @@ struct APIController: RouteCollection {
 		routes.get("api") { req -> String in
 			let routes = req.application.routes.all.filter { $0.path.starts(with: ["api"])}
 			return routes.map { route in
-				[route.method.string, route.path.map({$0.description}).joined(separator: "/"), (route.userInfo["description"] as? String)].compacted().joined(separator: "\t")
+				[route.method.rawValue, route.path.map({$0.description}).joined(separator: "/"), (route.userInfo["description"] as? String)].compacted().joined(separator: "\t")
 			}
 			.joined(separator: "\n")
 		}.description("This page. No auth required.")
