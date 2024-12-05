e902ad4cfd71c5f5fbbdd01a587dabab0d7f92e0
unbedenklich <info@unbedenklich.eu>
2024-12-05 22:38:02 +0100

    Update entrypoint to current Vapor template

--- Sources/App/entrypoint.swift
+++ Sources/App/entrypoint.swift
@@ -6,17 +6,17 @@ enum Entrypoint {
 	static func main() async throws {
 		var env = try Environment.detect()
 		try LoggingSystem.bootstrap(from: &env)
-		
+
 		let app = try await Application.make(env)
 
 		do {
 			try await configure(app)
 		} catch {
 			app.logger.report(error: error)
+			try? await app.asyncShutdown()
 			throw error
 		}
 		try await app.execute()
-
 		try await app.asyncShutdown()
 	}
 }
