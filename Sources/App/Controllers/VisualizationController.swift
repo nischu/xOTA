47cd008423782e76aeffe36cf3e758172ae78a89
unbedenklich <info@unbedenklich.eu>
2024-12-26 23:59:03 +0100

    Add option for auto-refresh and showing title.

--- Sources/App/Controllers/VisualizationController.swift
+++ Sources/App/Controllers/VisualizationController.swift
@@ -14,13 +14,20 @@ struct VisualizationController: RouteCollection {
 			var width: Int?
 			var height: Int?
 			var margin: Int
+			var refresh: Int?
+			var showTitle: Bool
 			var common: CommonContent
 		}
 		let showNav = try req.query.get(Bool?.self, at: "hideNav") != true
 		let width = try req.query.get(Int?.self, at: "width")
 		let height = try req.query.get(Int?.self, at: "height")
 		let margin = try req.query.get(Int?.self, at: "margin") ?? 50
+		var refresh = try req.query.get(Int?.self, at: "refresh")
+		if let refreshArg = refresh, refreshArg < 60 {
+			refresh = 60
+		}
+		let showTitle = try req.query.get(Bool?.self, at: "showTitle") ?? false
 
-		return try await req.view.render("viz-force-graph", Context(showNav:showNav, width: width, height: height, margin: margin, common: req.commonContent))
+		return try await req.view.render("viz-force-graph", Context(showNav:showNav, width: width, height: height, margin: margin, refresh: refresh, showTitle: showTitle, common: req.commonContent))
 	}
 }
